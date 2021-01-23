a546bab39f2d7648c9d6f2d71d7a7f109f221784
alec <alecbgarcia@gmail.com>
2021-01-22 20:29:24 -0700

    halfway through homescreen styling

---
+++ src/screens/Home/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { View, Text, StyleSheet, ImageBackground, Pressable, Dimensions } from 'react-native'
+import BG from '../../../assets/images/wallpaper.jpg'
+import Fontisto from 'react-native-vector-icons/Fontisto'
+
+export default function HomeScreen(props) {
+    return (
+        <View>
+            <ImageBackground source={BG} style={styles.background}>
+                <Pressable
+                    style={styles.searchButton}
+                    onPress={() => console.warn('Search Btn clicked')}
+                 >
+                 <View style={styles.searchbar}>
+                 <Fontisto name="search" size={25} color="#f15454" style={styles.searchIcon} />
+                 <Text style={styles.headerText}>Where are you going?</Text>
+                 </View>
+                 </Pressable>
+                <Text style={styles.title}>Go Near</Text>
+
+                <Pressable
+                    style={styles.button}
+                    onPress={() => console.warn('Expore Btn clicked')}
+                 >
+                 <Text style={styles.buttonText}>Explore nearby stays</Text>
+                 </Pressable>
+            </ImageBackground>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    background: {
+        width: '100%',
+        height: 500,
+        resizeMode: 'cover',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 100,
+        fontWeight: 'bold',
+        color: 'white',
+        width: '70%',
+        marginLeft: 15
+    },
+    buttonText: {
+        alignSelf: 'center',
+        fontSize: 12,
+        fontWeight: 'bold'
+    },
+    button: {
+        marginTop: 20,
+        backgroundColor: 'white',
+        width: 150,
+        marginLeft: 25,
+        height: 30,
+        borderRadius: 10,
+        justifyContent: 'center'
+    },
+    searchButton: {
+        backgroundColor: 'white',
+        width: Dimensions.get('screen').width - 20,
+        height: 30,
+        borderRadius: 10,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        position: 'absolute',
+        zIndex: 100,
+        top: 20,
+        marginHorizontal: 10,
+        borderRadius: 30,
+        height: 50
+    },
+    searchbar: {
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'space-around', 
+        width: 230
+    },
+    headerText: {
+        fontSize: 19
+    }
+})
